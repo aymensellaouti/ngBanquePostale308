b0fa46d83b6a82516080e926c5e19954f0757405
aymen2782 <aymen.sellaouti@gmail.com>
2023-08-30 14:13:16 +0100

    rotating card exercice correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FirstComponent } from './component/first/first.component';
-import { SecondComponent } from './component/second/second.component';
-import { ThirdComponent } from './component/third.component';
-import { ColorComponent } from './component/color/color.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FirstComponent } from "./component/first/first.component";
+import { SecondComponent } from "./component/second/second.component";
+import { ThirdComponent } from "./component/third.component";
+import { ColorComponent } from "./component/color/color.component";
+import { TwoComponent } from "./component/two/two.component";
+import { FormsModule } from "@angular/forms";
+import { RotatingCardComponent } from './component/rotating-card/rotating-card.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +17,12 @@ import { ColorComponent } from './component/color/color.component';
     FirstComponent,
     SecondComponent,
     ThirdComponent,
-    ColorComponent
+    ColorComponent,
+    TwoComponent,
+    RotatingCardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
